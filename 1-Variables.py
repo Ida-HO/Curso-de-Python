a054492bed22e2836f3346e323e8a5328df85f52
IDA HUENCHULAF <ida.huenchulaf@inacapmail.cl>
2025-08-15 19:20:31 -0400

    Add files via upload

---
+++ 1-Variables.py
@@ -0,0 +1,13 @@
+#VARIABLES
+
+#VARIABLE = VALOR (TIPO DE DATO NUMERICO, TEXTO Y BOLEANOS). 
+
+numero_entero = 10 #INT
+numero_flotante = 10.0 #FLOAT
+cadena = "Hola" #STR
+valor = True #BOOL
+
+print(numero_entero) #MUESTRA EL VALOR DE LA VARIABLE.
+print(type(cadena)) #FUNCION "type" MUESTRA LA ASIGNACION DEL VALOR DE LA VARIABLE.
+
+
