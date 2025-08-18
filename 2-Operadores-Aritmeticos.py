c5b2d8497400e264325ef79b3a19d1282501c133
IDA HUENCHULAF <ida.huenchulaf@inacapmail.cl>
2025-08-17 20:50:28 -0400

    Add files via upload

---
+++ 2-Operadores-Aritmeticos.py
@@ -0,0 +1,14 @@
+#Operadores Aritmeticos
+
+#Prioridades de los operadores aritmeticos:
+#Parentesis ()
+#Exponenciacion **
+#Multiplicacion *, Division /, Modulo %
+#Suma +, Resta -
+
+num1 = 10
+num2 = 5
+
+resultado = num1 + num2 
+
+print(resultado)
